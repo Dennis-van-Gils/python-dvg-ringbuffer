22ba3891f2fe32f8a2af819ce165f4eda1c6e98d
Dennis van Gils <37263907+Dennis-van-Gils@users.noreply.github.com>
2020-07-21 19:02:43 +0200

    Update test_dvg_ringbuffer.py

--- tests/test_dvg_ringbuffer.py
+++ tests/test_dvg_ringbuffer.py
@@ -25,25 +25,32 @@ class TestAll(unittest.TestCase):
     def test_append(self):
         r = RingBuffer(5)
 
+        self.assertTrue(r[0] is None)
+        self.assertTrue(r[-1] is None)
+
         r.append(1)
         np.testing.assert_equal(r, np.array([1]))
-        self.assertEqual(len(r), 1)
+        self.assertEqual(r[0], 1)
+        self.assertEqual(r[-1], 1)
 
         r.append(2)
         np.testing.assert_equal(r, np.array([1, 2]))
         self.assertEqual(len(r), 2)
+        self.assertEqual(r[0], 1)
+        self.assertEqual(r[-1], 2)
 
         r.append(3)
         r.append(4)
         r.append(5)
         np.testing.assert_equal(r, np.array([1, 2, 3, 4, 5]))
         self.assertEqual(len(r), 5)
+        self.assertEqual(r[0], 1)
+        self.assertEqual(r[-1], 5)
 
         r.append(6)
         np.testing.assert_equal(r, np.array([2, 3, 4, 5, 6]))
         self.assertEqual(len(r), 5)
-
-        self.assertEqual(r[4], 6)
+        self.assertEqual(r[0], 2)
         self.assertEqual(r[-1], 6)
 
     def test_getitem(self):
