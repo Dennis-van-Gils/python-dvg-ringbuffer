1ff2dd69d2f5c71e9882cf6e6e4a161868bfcad0
Dennis van Gils <37263907+Dennis-van-Gils@users.noreply.github.com>
2020-07-21 21:18:33 +0200

    Update tests

--- tests/test_dvg_ringbuffer.py
+++ tests/test_dvg_ringbuffer.py
@@ -25,39 +25,32 @@ class TestAll(unittest.TestCase):
     def test_append(self):
         r = RingBuffer(5)
 
-        self.assertTrue(r[0] is None)
-        self.assertTrue(r[-1] is None)
-
         r.append(1)
         np.testing.assert_equal(r, np.array([1]))
-        self.assertEqual(r[0], 1)
-        self.assertEqual(r[-1], 1)
+        self.assertEqual(len(r), 1)
 
         r.append(2)
         np.testing.assert_equal(r, np.array([1, 2]))
         self.assertEqual(len(r), 2)
-        self.assertEqual(r[0], 1)
-        self.assertEqual(r[-1], 2)
 
         r.append(3)
         r.append(4)
         r.append(5)
         np.testing.assert_equal(r, np.array([1, 2, 3, 4, 5]))
         self.assertEqual(len(r), 5)
-        self.assertEqual(r[0], 1)
-        self.assertEqual(r[-1], 5)
 
         r.append(6)
         np.testing.assert_equal(r, np.array([2, 3, 4, 5, 6]))
         self.assertEqual(len(r), 5)
-        self.assertEqual(r[0], 2)
+
+        self.assertEqual(r[4], 6)
         self.assertEqual(r[-1], 6)
 
     def test_getitem(self):
         r = RingBuffer(5)
-
-        r.extend([1, 2, 3, 4, 5, 6])
-        expected = np.array([2, 3, 4, 5, 6])
+        r.extend([1, 2, 3])
+        r.extendleft([4, 5])
+        expected = np.array([4, 5, 1, 2, 3])
         np.testing.assert_equal(r, expected)
 
         for i in range(r.maxlen):
@@ -66,20 +59,69 @@ class TestAll(unittest.TestCase):
         ii = [0, 4, 3, 1, 2]
         np.testing.assert_equal(r[ii], expected[ii])
 
-    def test_extend(self):
+    def test_appendleft(self):
         r = RingBuffer(5)
 
+        r.appendleft(1)
+        np.testing.assert_equal(r, np.array([1]))
+        self.assertEqual(len(r), 1)
+
+        r.appendleft(2)
+        np.testing.assert_equal(r, np.array([2, 1]))
+        self.assertEqual(len(r), 2)
+
+        r.appendleft(3)
+        r.appendleft(4)
+        r.appendleft(5)
+        np.testing.assert_equal(r, np.array([5, 4, 3, 2, 1]))
+        self.assertEqual(len(r), 5)
+
+        r.appendleft(6)
+        np.testing.assert_equal(r, np.array([6, 5, 4, 3, 2]))
+        self.assertEqual(len(r), 5)
+
+    def test_extend(self):
+        r = RingBuffer(5)
         r.extend([1, 2, 3])
         np.testing.assert_equal(r, np.array([1, 2, 3]))
-
+        r.popleft()
         r.extend([4, 5, 6])
         np.testing.assert_equal(r, np.array([2, 3, 4, 5, 6]))
+        r.extendleft([0, 1])
+        np.testing.assert_equal(r, np.array([0, 1, 2, 3, 4]))
+
+        r.extendleft([1, 2, 3, 4, 5, 6, 7])
+        np.testing.assert_equal(r, np.array([1, 2, 3, 4, 5]))
 
         r.extend([1, 2, 3, 4, 5, 6, 7])
         np.testing.assert_equal(r, np.array([3, 4, 5, 6, 7]))
 
+    def test_pops(self):
+        r = RingBuffer(3)
+        r.append(1)
+        r.appendleft(2)
+        r.append(3)
+        np.testing.assert_equal(r, np.array([2, 1, 3]))
+
+        self.assertEqual(r.pop(), 3)
+        np.testing.assert_equal(r, np.array([2, 1]))
+
+        self.assertEqual(r.popleft(), 2)
+        np.testing.assert_equal(r, np.array([1]))
+
+        # test empty pops
+        empty = RingBuffer(1)
+        with self.assertRaisesRegex(
+            IndexError, "Pop from an empty RingBuffer."
+        ):
+            empty.pop()
+        with self.assertRaisesRegex(
+            IndexError, "Pop from an empty RingBuffer."
+        ):
+            empty.popleft()
+
     def test_2d(self):
-        r = RingBuffer(3, dtype=(np.float, 2))
+        r = RingBuffer(5, dtype=(np.float, 2))
 
         r.append([1, 2])
         np.testing.assert_equal(r, np.array([[1, 2]]))
@@ -91,19 +133,14 @@ class TestAll(unittest.TestCase):
         self.assertEqual(len(r), 2)
         self.assertEqual(np.shape(r), (2, 2))
 
-        r.append([5, 6])
-        np.testing.assert_equal(r, np.array([[1, 2], [3, 4], [5, 6]]))
+        r.appendleft([5, 6])
+        np.testing.assert_equal(r, np.array([[5, 6], [1, 2], [3, 4]]))
         self.assertEqual(len(r), 3)
         self.assertEqual(np.shape(r), (3, 2))
 
-        r.append([7, 8])
-        np.testing.assert_equal(r, np.array([[3, 4], [5, 6], [7, 8]]))
-        self.assertEqual(len(r), 3)
-        self.assertEqual(np.shape(r), (3, 2))
-
-        np.testing.assert_equal(r[0], [3, 4])
-        np.testing.assert_equal(r[0, :], [3, 4])
-        np.testing.assert_equal(r[:, 0], [3, 5, 7])
+        np.testing.assert_equal(r[0], [5, 6])
+        np.testing.assert_equal(r[0, :], [5, 6])
+        np.testing.assert_equal(r[:, 0], [5, 1, 3])
 
     def test_iter(self):
         r = RingBuffer(5)
@@ -123,12 +160,24 @@ class TestAll(unittest.TestCase):
         r = RingBuffer(3, allow_overwrite=False)
         r.append(1)
         r.append(2)
-        r.append(3)
+        r.appendleft(3)
+        with self.assertRaisesRegex(IndexError, "overwrite"):
+            r.appendleft(4)
+        with self.assertRaisesRegex(IndexError, "overwrite"):
+            r.extendleft([4])
+        r.extendleft([])
+
+        np.testing.assert_equal(r, np.array([3, 1, 2]))
         with self.assertRaisesRegex(IndexError, "overwrite"):
             r.append(4)
         with self.assertRaisesRegex(IndexError, "overwrite"):
             r.extend([4])
         r.extend([])
+
+        # works fine if we pop the surplus
+        r.pop()
+        r.append(4)
+        np.testing.assert_equal(r, np.array([3, 1, 4]))
 
     def test_degenerate(self):
         r = RingBuffer(0)
@@ -137,6 +186,7 @@ class TestAll(unittest.TestCase):
         # this does not error with deque(maxlen=0), so should not error here
         try:
             r.append(0)
+            r.appendleft(0)
         except IndexError:
             self.fail()
 
